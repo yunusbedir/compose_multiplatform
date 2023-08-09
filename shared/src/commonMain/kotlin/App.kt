a1edfdf8d98882a54bbf137951c206d8565b34e1
yunusbedir <yunus.bedir@btc-ag.com.tr>
2023-08-09 16:49:00 +0300

    custom material theme

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -17,6 +18,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
@@ -26,8 +28,22 @@ import io.kamel.image.asyncPainterResource
 import model.BirdImage
 
 @Composable
+fun BirdAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp),
+        )
+    ) {
+        content()
+    }
+}
+
+@Composable
 fun App() {
-    MaterialTheme {
+    BirdAppTheme {
         val birdsViewModel: BirdsViewModel =
             getViewModel(Unit, viewModelFactory { BirdsViewModel() })
         BirdsPage(
@@ -56,7 +72,8 @@ fun BirdsPage(viewModel: BirdsViewModel) {
                 Button(
                     onClick = {
                         viewModel.selectCategory(category)
-                    }
+                    },
+                    modifier = Modifier.aspectRatio(1.0f).fillMaxSize().weight(1.0f)
                 ) {
                     Text(category)
                 }
